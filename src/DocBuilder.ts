941c723140c9c50f42a423bce3a9522a82b600ea
Ian McNicoll <ian@freshehr.com>
2021-04-05 16:10:48 +0100

    Add RM attributes and participation support

--- src/DocBuilder.ts
+++ src/DocBuilder.ts
@@ -7,7 +7,7 @@ import { FormList } from './FormList';
 
 export class DocBuilder {
   sb: StringBuilder = new StringBuilder();
-  defaultLang: string = 'nb';
+  defaultLang: string = 'en';
 
   constructor(private wt: WebTemplate) {
     this.defaultLang = wt.defaultLanguage;
@@ -18,17 +18,17 @@ export class DocBuilder {
   }
 
   private generate() {
-    // this.sb.append(`= ${this.wt.templateId}`);
+
     const f = this.wt.tree;
 
-    this.sb.append(`= ${f.name}`).newline();
-    this.sb.append(`== Metadata`).newline();
-    this.sb.append(`TemplateId:: ${this.wt.templateId}`).newline();
-    this.sb.append(`Archetype:: ${f.nodeId}`).newline();
-    this.sb.append(`Root name:: ${f.name}`).newline();
-    this.sb.newline();
+    this.sb.append(`= ${this.wt.templateId}`).newline();
+    this.sb.append(`== *${f.name}*`).newline()
+    this.sb.append(`=== \`${f.rmType}: _${f.nodeId}_\``).newline();
+
     this.walk(this.wt.tree);
   }
+
+
   private walkChildren(f: FormElement) {
     if (f.children) {
       f.children.forEach((child) => {
@@ -36,9 +36,18 @@ export class DocBuilder {
       });
     }
   }
+
+  private walkNonContextChildren(f: FormElement) {
+    if (f.children) {
+      f.children.forEach((child) => {
+        if (!child.inContext) {this.walk(child)}
+      });
+    }
+  }
+
   private walk(f: FormElement) {
     if (f.aqlPath === '/category') {
-      // skipping category
+      this.walkChildren(f);
     } else if (isEntry(f.rmType)) {
       this.walkEntry(f);
     } else if (isDataValue(f.rmType)) {
@@ -46,28 +55,34 @@ export class DocBuilder {
     } else if (isSection(f.rmType)) {
       this.walkSection(f);
     } else if (f.rmType === 'CLUSTER') {
-      this.sb.append(`5+a|*${f.name}* + \n${f.rmType}: _${f.nodeId}_`);
+      this.sb.append(`5+a|*${f.name}* +\n \`${f.rmType}: _${f.nodeId}_\``);
       this.walkChildren(f);
     } else {
       switch (f.rmType) {
-        case 'COMPOSITIION':
-          this.walkChildren(f);
+        case 'COMPOSITION':
+          this.walkRmAttributes(f)
+          this.walkNonContextChildren(f);
           break;
         case 'ISM_TRANSITION':
           this.walkChildren(f);
           break;
         case 'EVENT_CONTEXT':
-          f.name = 'Context';
+          f.name = 'context';
           this.walkEntry(f);
           break;
         case 'CODE_PHRASE':
-          f.name = 'CODE_PHRASE';
-          this.walkChildren(f);
+          f.name = f.id;
+          this.walkElement(f);
+          break;
+        case 'PARTY_PROXY':
+          f.name = f.id;
+          this.walkElement(f);
           break;
 
         default:
           this.sb.append('// Not supported rmType ' + f.rmType);
-          this.walkChildren(f);
+
+   //       this.walkChildren(f);
           break;
       }
     }
@@ -82,32 +97,86 @@ export class DocBuilder {
   }
   private walkEntry(f: FormElement) {
 
-    const nodeId = f.nodeId?f.nodeId:`RM:${f.id}`
+
+    const nodeId = f.nodeId?f.nodeId:`\`RM:${f.id}\``
 
     this.sb.append(`=== ${f.name}`);
-    this.sb.append('[options="header", cols="3,3,5,5,30"]');
-    this.sb.append('|====');
-    this.sb.append('|NodeId|Attr.|RM Type| Name | Description');
-    this.sb.append(`5+a|*${f.name}* + \n${f.rmType}: _${nodeId}_`);
+    this.sb.append(`===== \`${f.rmType}: _${nodeId}_\``);
+
+    this.walkRmAttributes(f);
+
     if (f.children) {
+
+      this.sb.append('[options="header", cols="5,3,5,5,30"]');
+      this.sb.append('|====');
+      this.sb.append('|NodeId|Attr.|RM Type| Name | Description');
+
+      f.children.forEach((child) => {
+        if (!child.inContext) {
+          this.walk(child)
+        }
+      });
+
+      this.sb.append('|====');
+
+    }
+
+
+
+  }
+
+  private walkRmAttributes(f: FormElement) {
+
+    const rmAttributes = new Array <FormElement>();
+
+    if (f.children) {
+
       f.children.forEach((child) => {
         if (child.inContext !== undefined && child.inContext) {
-          // Will not document things in contexts (which is included in openEHR RM)
-          this.walk(child);
-        } else {
-          this.walk(child);
+          rmAttributes.push(child);
         }
+      });
+
+    }
+
+    if (rmAttributes.length > 0) {
+      this.sb.append(`===== RM attributes`);
+      this.sb.append('[options="header", cols="5,3,5,5,30"]');
+      this.sb.append('|====');
+      this.sb.append('|NodeId|Attr.|RM Type| Name | Description');
+
+      rmAttributes.forEach((child) => {
+        this.walk(child);
       });
       this.sb.append('|====');
     }
+
+    this.walkParticipations(true);
+
   }
+
+  private walkParticipations(displayParticipations : boolean) {
+
+    if (displayParticipations) {
+      this.sb.append(`===== _Participations_ [0..*]`);
+      this.sb.append('[options="header", cols="5,3,5,5,30"]');
+      this.sb.append('|====');
+      this.sb.append('|NodeId|Attr.|RM Type| Name | Description');
+      this.sb.append('|RM: function|1..1|DV_TEXT| Role | The function of the Party in this participation');
+//      this.sb.append('|RM: mode|0..1|DV_CODED_TEXT| Mode | Optional field for recording the \'mode\' of the performer / activity interaction, e.g. present, by telephone, by email etc.');
+      this.sb.append('|RM: performer|1..1|PARTY_IDENTIFIED| Performer name and ID | The id and possibly demographic system link of the party participating in the activity.');
+//      this.sb.append('|RM: time|0..1|DV_INTERVAL| Time | The time interval during which the participation took place, ');
+      this.sb.append('|====');
+    }
+  }
+
   private walkElement(f: FormElement) {
     const max = f.max < 0 ? '*' : `${f.max}`;
 
-    const nodeId = f.nodeId?f.nodeId:`RM: ${f.id}`;
-    this.sb.append(`|${nodeId}| ${f.min}..${max}| ${f.rmType} | ${f.name}`);
+    const nodeId = f.nodeId?f.nodeId:`RM:`;
+    this.sb.append(`|${nodeId} + \n \`${f.id}\`| ${f.min}..${max}| ${f.rmType} | ${f.name}`);
 
-    if (f.name === undefined) this.sb.append(`// ${f.id} -  ${f.aqlPath}`);
+    if (f.name === undefined){this.sb.append(`// ${f.id} -  ${f.aqlPath}`);}
 
     switch (f.rmType) {
       case 'DV_CODED_TEXT':
@@ -141,15 +210,22 @@ export class DocBuilder {
       case 'DV_IDENTIFIER':
         this.walkDvIdentifier(f);
         break;
+        break;
+      case 'CODE_PHRASE':
+        this.walkCodePhrase(f);
+        break;
+        case 'PARTY_PROXY':
+        this.walkPartyProxy(f);
+        break;
+
       default:
-        this.sb.append('|Not supported rm type' + f.rmType);
+        this.sb.append('|Not supported rm type: ' + f.rmType);
     }
 
     if (f.annotations) {
-      this.sb.newline();
-      this.sb.append(f.annotations.comment);
+      this.sb.newline().append(`Comment: ${f.annotations.comment}`);
     }
-    // this.sb.append(`${this.getValueOfRecord(f.localizedDescriptions)}`);
+
   }
 
   private walkDvBoolean(f: FormElement) {
@@ -158,7 +234,10 @@ export class DocBuilder {
 
   private walkCodePhrase(f: FormElement) {
     this.sb.append('|');
-    this.sb.append('Code_phrase')
+  }
+
+  private walkPartyProxy(f: FormElement) {
+   this.sb.append('|');
   }
 
   private walkDvIdentifier(f: FormElement) {
@@ -207,7 +286,9 @@ export class DocBuilder {
         }
         if (item.listOpen)
           this.sb.append( `* _Other text allowed_`);
+
       });
+      this.sb.newline().append(`${this.getValueOfRecord(f.localizedDescriptions)}`);
     }
   }
   private walkDvDateTime(f: FormElement) {
@@ -228,6 +309,7 @@ export class DocBuilder {
           });
           if (item.listOpen)
             this.sb.append( `* _Other text allowed_`);
+          this.sb.newline().append(`${this.getValueOfRecord(f.localizedDescriptions)}`);
         }
       });
     }
